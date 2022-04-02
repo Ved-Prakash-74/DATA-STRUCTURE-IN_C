101b84b3732e2990b7e7dd02074a7bee6c52a558
Ved-Prakash-74 <96818066+Ved-Prakash-74@users.noreply.github.com>
2022-04-02 20:49:17 +0530

    Create inserting_at_rear_end.c

---
+++ QUEUE/DOUBLE ENDED QUEUE/DOUBLE ENDED QUEUE USING ARRAY/inserting_at_rear_end.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 10
+int queue[size];
+int rear=-1,front=-1;
+void enque();
+void insert_at_rear_end();
+void display();
+int main()
+{
+    printf("\nDOUBLE ENDED QUEUE USING ARRAYS");
+    enque();
+    insert_at_rear_end();
+    display();
+    return 0;
+}
+void enque()
+{
+    char ch;
+    do
+    {
+      int ele;
+      if((front==0 && rear==size-1)||front==rear+1)
+      {
+          printf("\nQueue is full or OVERFLOW");
+      }
+      else
+      {
+          rear=(rear+1)%size;
+          printf("\nEnter element: ");
+          scanf("%d",&ele);
+          queue[rear]=ele;
+          if(front==-1)
+          {
+              front=0;
+          }
+      }
+      printf("\nDo you want to add more elements in queue: ");
+      fflush(stdin);
+      scanf("%c",&ch);
+    }while(ch=='Y'||ch=='y');
+}
+void insert_at_rear_end()
+{
+    int ele;
+    if(rear==size-1)
+    {
+       printf("\nQueue is full or OVERFLOW");
+    }
+    else
+    {
+        rear++;
+        printf("\nEnter element which is to be inserted at rear end: ");
+        scanf("%d",&ele);
+        queue[rear]=ele;
+        if(front==-1)
+        {
+            front=0;
+        }
+    }
+}
+void display()
+{
+    int i;
+    if(front==-1 && rear==-1)
+    {
+        printf("\nQueue is empty or UNDERFLOW");
+    }
+    else
+    {
+        printf("\nElements of queue are: ");
+      for(i=front;i!=rear;i=(i+1)%size)
+      {
+          printf("\n%d",queue[i]);
+      }
+      printf("\n%d",queue[i]);
+    }
+}
