c479c987c4dff10c0575fbac78184c061fabb16f
Ved-Prakash-74 <96818066+Ved-Prakash-74@users.noreply.github.com>
2022-04-02 20:51:23 +0530

    Create deleting_from_rear_end.c

---
+++ QUEUE/DOUBLE ENDED QUEUE/DOUBLE ENDED QUEUE USING ARRAY/deleting_from_rear_end.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 10
+int queue[size];
+int rear=-1,front=-1;
+void enque();
+void delete_from_rear_end();
+void display();
+int main()
+{
+    printf("\nDOUBLE ENDED QUEUE USING ARRAYS");
+    enque();
+    delete_from_rear_end();
+    display();
+    return 0;
+}
+void enque()
+{
+    char ch;
+    do
+    {
+      int ele;
+      if((front==0 && rear==size-1)||front==rear+1)
+      {
+          printf("\nQueue is full or OVERFLOW");
+      }
+      else
+      {
+          rear=(rear+1)%size;
+          printf("\nEnter element: ");
+          scanf("%d",&ele);
+          queue[rear]=ele;
+          if(front==-1)
+          {
+              front=0;
+          }
+      }
+      printf("\nDo you want to add more elements in queue: ");
+      fflush(stdin);
+      scanf("%c",&ch);
+    }while(ch=='Y'||ch=='y');
+}
+void delete_from_rear_end()
+{
+    if(front==-1 && rear==-1)
+    {
+       printf("\nQueue is empty or UNDERFLOW");
+    }
+    else if(front==rear)
+    {
+      printf("\nDeleted element is: %d",queue[rear]);
+      front=-1;
+      rear=-1;
+    }
+    else
+    {
+      printf("\nDeleted element is: %d",queue[rear]);
+      rear--;
+    }
+}
+void display()
+{
+    int i;
+    if(front==-1 && rear==-1)
+    {
+        printf("\nQueue is empty or UNDERFLOW");
+    }
+    else
+    {
+        printf("\nElements of queue are: ");
+
+      for(i=front;i!=rear;i=(i+1)%size)
+      {
+          printf("\n%d",queue[i]);
+      }
+      printf("\n%d",queue[i]);
+    }
+}
+
